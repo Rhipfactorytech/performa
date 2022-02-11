c3f080549580d5d97150d5b90d8a39867dd40a13
emohex <olumidemm@gmail.com>
2022-02-11 12:14:18 +0100

    admin view partners check ins

---
+++ pages/admin/dashboard/monthly/_details/_detailsc.vue
@@ -0,0 +1,433 @@
+<template>
+  <div>
+    <v-row class="mt-4 mx-auto">
+      <v-card width="100%" outlined height="1100">
+        <v-row class="mx-4 mt-4">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="are you happy here and why?"
+            class="titl-fnt-mb-b3"
+            v-model="happy"
+            readonly
+          ></v-textarea>
+        </v-row>
+        <v-row class="mx-4 mt-2">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="what have you accomplished thus far and what are you most proud
+              of?"
+            class="titl-fnt-mb-b3"
+            v-model="accomp"
+            readonly
+          ></v-textarea>
+        </v-row>
+        <v-row class="mx-4 mt-2">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="what are your challenges and concerns?"
+            class="titl-fnt-mb-b3"
+            v-model="chall"
+            readonly
+          ></v-textarea>
+        </v-row>
+        <v-row class="mx-4 mt-2">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="how would you rate your performace till date and why?"
+            class="titl-fnt-mb-b3"
+            v-model="perf"
+            readonly
+          ></v-textarea>
+        </v-row>
+        <v-row class="mx-4 mt-2">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="How do you suggest your role coaches help you get better"
+            class="titl-fnt-mb-b3"
+            v-model="sugg"
+            readonly
+          ></v-textarea>
+        </v-row>
+        <v-row class="mx-4 mt-2">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="and other feedback?"
+            class="titl-fnt-mb-b3"
+            v-model="feedb"
+            readonly
+          ></v-textarea>
+        </v-row>
+      </v-card>
+    </v-row>
+
+      <v-row class="mt-10 mx-auto mb-4">
+      <v-col cols="6" class="mr-auto">
+        <v-row>
+          <span class="titl-fnt-mb-b3"> Role Coach 1: {{ getCoach1 }} </span>
+        </v-row>
+        <v-row>
+          <v-card width="98%" outlined height="400">
+            <v-row class="pt-8">
+              <v-textarea
+                outlined
+                name="input-6-4"
+                label="feedback"
+                class="titl-fnt-mb-b3 mx-8"
+                v-model="fcoach1"
+                readonly
+              ></v-textarea>
+            </v-row>
+            <v-row class="mt-4">
+              <v-textarea
+                outlined
+                name="input-6-4"
+                label="Recommendation"
+                class="titl-fnt-mb-b3 mx-8"
+                v-model="fcoach1R"
+                readonly
+              ></v-textarea>
+            </v-row>
+          </v-card>
+        </v-row>
+      </v-col>
+      <v-col cols="6" class="ml-auto">
+        <v-row>
+          <span class="titl-fnt-mb-b3"> Role Coach 2: {{ getCoach2 }} </span>
+        </v-row>
+        <v-row>
+          <v-card width="100%" outlined height="400">
+            <v-row class="pt-8">
+              <v-textarea
+                outlined
+                name="input-6-4"
+                label="feedback"
+                class="titl-fnt-mb-b3 mx-8"
+                v-model="fcoach2"
+                readonly
+              ></v-textarea>
+            </v-row>
+            <v-row class="mt-4">
+              <v-textarea
+                outlined
+                name="input-6-4"
+                label="Recommendation"
+                class="titl-fnt-mb-b3 mx-8"
+                v-model="fcoach2R"
+                readonly
+              ></v-textarea>
+            </v-row>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-snackbar v-model="snackbar" :timeout="timeout" color="success" top>
+      {{ msg }}
+    </v-snackbar>
+    <v-snackbar v-model="snackbarErr" :timeout="timeout" color="error" top>
+      {{ msg }}
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+import { format, parseISO } from 'date-fns'
+import axios from 'axios'
+import { mapState } from 'vuex'
+import { validationMixin } from 'vuelidate'
+import { required, minLength, email, sameAs } from 'vuelidate/lib/validators'
+export default {
+  layout: 'dashboard',
+  mixins: [validationMixin],
+  validations: {
+    //  weekUP: { required },
+    //dateRangeText: { required },
+  },
+
+  data() {
+    return {
+      btnM:"view your feedback",
+      viewFeedback: false,
+      coach1f: '',
+      coach1R: '',
+      coach1fview: '',
+      coach1Rview: '',
+      happy: '',
+      accomp: '',
+      chall: '',
+      perf: '',
+      sugg: '',
+      feedb: '',
+      fcoach1: '',
+      fcoach2: '',
+      fcoach1R: '',
+      fcoach2R: '',
+      timeout: 7000,
+      msg: '',
+      modal: false,
+      weekUP: '',
+      menu1: false,
+      menu2: false,
+      goal: '',
+      path: '',
+      knowledgeArea: '',
+      dateRangeText: '',
+      dialog: false,
+      hide: false,
+      name: '',
+      duration: '',
+      objective: '',
+      editmode: true,
+      id: this.$route.params.detailsc,
+      headers: [
+        {
+          text: 'Partner name',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Stream', value: 'tribe' },
+        { text: 'Designation', value: 'designation' },
+        { text: 'Status', value: 'status' },
+        { text: 'view profile' },
+      ],
+      body: [
+        {
+          name: 'olumide',
+        },
+      ],
+      loading: false,
+      msg: '',
+      timeout: 7000,
+      snackbar: false,
+      snackbarErr: false,
+      name: '',
+      workid: '',
+      email: '',
+      resumption: '',
+      phone: '',
+      stream: '',
+      designation: '',
+      currentSkill: '',
+      nextskill: '',
+      rolecoach: '',
+      tab: null,
+      ref: '',
+      new_data: '',
+      e1: 1,
+      email: '',
+      address: '',
+      phoneNumber: '',
+      name: '',
+      date: '',
+      menu2: false,
+      appraisal: [],
+      body: [],
+      messages: [],
+      dates: [],
+      getCoach1: '',
+      getCoach2: '',
+      monthlyid: '',
+      coachname: '',
+    }
+  },
+  methods: {
+    async sendFeedback() {
+      this.loading = true
+      if (this.coachname == this.getCoach1) {
+        try {
+          const res = await axios.post(
+            `${this.$config.baseUrl}admin/godmode/monthlycheckin/coach/review/${this.$route.query.monthid}`,
+            {
+              feedbackCoach1: this.coach1f,
+              recommendationCoach1: this.coach1R,
+            }
+          )
+          console.log(res)
+          this.loading = false
+          this.msg = res.data.msg
+          this.snackbar = true
+        } catch (error) {
+          console.log(error.response.data.detail)
+          this.loading = false
+          this.msg = error.response.data.detail
+          this.snackbarErr = true
+        }
+      } else if (this.coachname == this.getCoach2) {
+        try {
+          const res = await axios.post(
+            `${this.$config.baseUrl}admin/godmode/monthlycheckin/coach/review/${this.$route.query.monthid}`,
+            {
+              feedbackCoach2: this.coach1f,
+              recommendationCoach2: this.coach1R,
+            }
+          )
+          console.log(res)
+          this.loading = false
+          this.msg = res.data.msg
+          this.snackbar = true
+        } catch (error) {
+          console.log(error.response.data.detail)
+          this.loading = false
+          this.msg = error.response.data.detail
+          this.snackbarErr = true
+        }
+      }
+    },
+    async getmessages() {
+      try {
+        const res = await this.$axios.$get(
+          `${this.$config.baseUrl}partner/monthlycheck/get/${this.$route.query.monthid}`
+        )
+        console.log(res)
+        this.happy = res.data.areyouhappy
+        this.accomp = res.data.accomplishment
+        this.chall = res.data.challenges
+        this.perf = res.data.performance
+        this.sugg = res.data.suggestion
+        this.feedb = res.data.feedback
+        this.fcoach1 = res.data.feedbackCoach1
+        this.fcoach2 = res.data.feedbackCoach2
+        this.fcoach1R = res.data.recommendationCoach1
+        this.fcoach2R = res.data.recommendationCoach2
+        this.monthlyid = res.data.monthlyId
+      } catch (error) {
+        console.log(error.response)
+      }
+    },
+    async getmessagesfeedback() {
+      if (this.coachname == this.getCoach1) {
+        try {
+          const res = await this.$axios.$get(
+            `${this.$config.baseUrl}partner/monthlycheck/get/${this.$route.query.monthid}`
+          )
+          this.coach1fview = res.data.feedbackCoach1
+
+          this.coach1Rview = res.data.recommendationCoach1
+
+          this.monthlyid = res.data.monthlyId
+        } catch (error) {
+          console.log(error.response)
+        }
+      } else if (this.coachname == this.getCoach2) {
+        try {
+          const res = await this.$axios.$get(
+            `${this.$config.baseUrl}partner/monthlycheck/get/${this.$route.query.monthid}`
+          )
+
+          this.coach1fview = res.data.feedbackCoach2
+
+          this.coach1Rview = res.data.recommendationCoach2
+          this.monthlyid = res.data.monthlyId
+        } catch (error) {
+          console.log(error.response)
+        }
+      }
+      if (this.viewFeedback == false) {
+        this.viewFeedback = true
+        this.btnM = "Give feedback"
+      } else if (this.viewFeedback == true) {
+        this.viewFeedback = false
+        this.btnM = "view your feedback"
+      }
+    },
+    async getPartners() {
+      try {
+        const res = await axios.get(
+          `${this.$config.baseUrl}admin/godmode/emp/view/${this.$route.query.partnerId}`
+        )
+        console.log(res)
+        // this.body = res.data.data
+        this.name = res.data.data.name
+        this.workid = res.data.data.workid
+        this.getWorkId = res.data.data.workid
+        this.resumption = res.data.data.dateofresumption
+        this.stream = res.data.data.tribe
+        this.designation = res.data.data.designation
+        this.getCoach1 = res.data.data.rolecoach1
+        this.getCoach2 = res.data.data.rolecoach2
+      } catch (error) {
+        console.log(error.response)
+      }
+    },
+    async getCoach() {
+      try {
+        const res = await this.$axios.$get(
+          `${this.$config.baseUrl}partner/coach/viewprofile`
+        )
+        console.log(res)
+        // this.body = res.data.data
+        this.coachname = res.data.name
+      } catch (error) {
+        console.log(error.response)
+      }
+    },
+    test() {
+      console.log(this.getCoach1)
+      if (this.coachname == this.getCoach1) {
+        console.log('na role coach 1 be this')
+      } else if (this.coachname == this.getCoach2) {
+        console.log('na role coach 2 be this')
+      }
+    },
+  },
+  created() {
+    this.getmessages()
+    this.getPartners()
+   // this.getCoach()
+    //this.getmessagesfeedback()
+    // this.test()
+  },
+  filters: {
+    formatdate(value) {
+      if (value == true) {
+        return 'Active'
+      } else {
+        return 'disabled'
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@500&display=swap');
+.titl-fnt-mb {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 20px;
+  font-weight: lighter;
+  color: #5465ff;
+}
+
+.titl-fnt-mb-b {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 15px;
+  font-weight: bold;
+  color: #5465ff;
+}
+
+.titl-fnt-mb-b2 {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 15px;
+}
+
+.titl-fnt-mb-b3 {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 12px;
+}
+.titl-fnt {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 10px;
+  color: #5465ff;
+}
+.v-text-field {
+  width: 20px;
+}
+</style>
---
+++ pages/admin/dashboard/monthly/index.vue
@@ -0,0 +1,235 @@
+<template>
+  <div>
+    <v-simple-table class="mt-4">
+      <template v-slot:default>
+        <thead>
+          <tr class="titl-fnt-mb-b3">
+            <th class="text-left">
+              <span class="titl-fnt-mb-b3">Partner Name</span>
+            </th>
+            <th class="text-left">
+              <span class="titl-fnt-mb-b3">Stream</span>
+            </th>
+            <th class="text-left">
+              <span class="titl-fnt-mb-b3">SubStream </span>
+            </th>
+            <th class="text-left">
+              <span class="titl-fnt-mb-b3">View monthly check-ins </span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in body" :key="item.workid" class="mt-2">
+            <td>
+              <span class="titl-fnt-mb-b2">{{ item.name }} </span>
+            </td>
+            <td>
+              <span class="titl-fnt-mb-b2">{{ item.tribe }}</span>
+            </td>
+            <td>
+              <span class="titl-fnt-mb-b2">{{ item.designation }}</span>
+            </td>
+            <!--  <td>
+              <span class="titl-fnt-mb-b2">{{ item.action | formatdate }}</span>
+            </td> -->
+            <td>
+              <span class="titl-fnt-mb-b2">
+                <nuxt-link :to="'/admin/dashboard/monthly/' + item.workId">
+                  view profile
+                </nuxt-link>
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+
+    
+
+    <v-snackbar v-model="snackbar" :timeout="timeout" color="success" top>
+      {{ msg }}
+    </v-snackbar>
+    <v-snackbar v-model="snackbarErr" :timeout="timeout" color="error" top>
+      {{ msg }}
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapState } from 'vuex'
+import { validationMixin } from 'vuelidate'
+import { required, minLength, email, sameAs } from 'vuelidate/lib/validators'
+import Business from '../../../../components/Business.vue'
+export default {
+  components: { Business },
+  layout: 'dashboard',
+  mixins: [validationMixin],
+  validations: {
+    email: { required },
+
+    phoneNumber: { required },
+    name: { required },
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Partner name',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Stream', value: 'tribe' },
+        { text: 'Designation', value: 'designation' },
+        { text: 'Status', value: 'status' },
+        { text: 'view profile' },
+      ],
+      body: [],
+      loading: false,
+      msg: '',
+      timeout: 7000,
+      snackbar: false,
+      snackbarErr: false,
+      name: '',
+      workid: '',
+      email: '',
+      resumption: '',
+      phone: '',
+      stream: '',
+      designation: '',
+      currentSkill: '',
+      nextskill: '',
+      rolecoach: '',
+      tab: null,
+      ref: '',
+      new_data: '',
+      e1: 1,
+      email: '',
+      address: '',
+      phoneNumber: '',
+      name: '',
+      picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      menu2: false,
+    }
+  },
+  computed: {
+    nameError() {
+      const errors = []
+      if (!this.$v.name.$dirty) return errors
+      !this.$v.name.required && errors.push('name is required')
+      return errors
+    },
+    emailError() {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.required && errors.push('email is required')
+      return errors
+    },
+    addressError() {
+      const errors = []
+      if (!this.$v.address.$dirty) return errors
+      !this.$v.address.required && errors.push('Address is required')
+      return errors
+    },
+    phoneNumberError() {
+      const errors = []
+      if (!this.$v.phoneNumber.$dirty) return errors
+      !this.$v.phoneNumber.required && errors.push('Phone number is required')
+      return errors
+    },
+  },
+  methods: {
+    async getPartners() {
+      try {
+        const res = await this.$axios.$get(
+          `${this.$config.baseUrl}admin/godmode/emp/viewall`
+        )
+        console.log(res.data)
+        this.body = res.data
+      } catch (error) {
+        console.log(error.response)
+      }
+    },
+    async add_partner() {
+      this.loading = true
+      try {
+        const res = await axios.post(
+          `${this.$config.baseUrl}admin/godmode/emp/create`,
+          {
+            name: this.name,
+            workid: this.workid,
+            workemail: this.email,
+            designation: this.designation,
+            tribe: this.stream,
+            phonenumber: this.phone,
+            dateOfresumption: this.resumption,
+            currentSkill: this.currentSkill,
+            nextSkill: this.nextskill,
+            roleCoach: this.rolecoach,
+          }
+        )
+        console.log(res)
+        this.loading = false
+        this.msg = res.data.msg
+        this.snackbar = true
+      } catch (error) {
+        console.log(error.response)
+        this.loading = false
+        this.msg = error.response.data.detail
+        this.snackbarErr = true
+      }
+    },
+  },
+  created() {
+    this.getPartners()
+  },
+  filters: {
+    formatdate(value) {
+      if (value == true) {
+        return 'Active'
+      } else {
+        return 'disabled'
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@500&display=swap');
+.titl-fnt-mb {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 20px;
+  font-weight: lighter;
+  color: #5465ff;
+}
+
+.titl-fnt-mb-b {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 20px;
+  font-weight: bold;
+  color: #5465ff;
+}
+
+.titl-fnt-mb-b2 {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 13px;
+}
+
+.titl-fnt-mb-b3 {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 15px;
+}
+.titl-fnt {
+  font-family: 'Urbanist', sans-serif;
+  font-size: 15px;
+  color: #585960;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
